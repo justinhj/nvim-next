aa97478f261ec1fc8a61ac72be18c5b12eda4d3e
Justin Heyes-Jones <justinhj@gmail.com>
2026-04-05 11:52:08 -0700

    Get it started

---
+++ lua/util/util.lua
@@ -0,0 +1,15 @@
+-- Place for useful utils
+
+local M = {}
+
+-- This improves the 0 key handler
+function M.toggleZeroMovement()
+	vim.api.nvim_set_keymap(
+		"n",
+		"0",
+		"getline('.')[0 : col('.') - 2] =~# '^\\s\\+$' ? '0' : '^'",
+		{ silent = true, noremap = true, expr = true }
+	)
+end
+
+return M
