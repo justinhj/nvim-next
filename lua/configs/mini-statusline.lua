3a5ff35a5a409f9447b1fb2e74e8c6866095a773
Justin Heyes-Jones <justinhj@gmail.com>
2026-04-16 20:30:46 -0700

    Some statusline modernization

--- lua/configs/mini-statusline.lua
+++ lua/configs/mini-statusline.lua
@@ -5,22 +5,23 @@ statusline.setup({
     active = function()
       local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
       local git           = statusline.section_git({ trunc_width = 75 })
-      local diff          = statusline.section_diff({ trunc_width = 75 })
-      local diagnostics   = statusline.section_diagnostics({ trunc_width = 75 })
+      local diagnostics   = vim.diagnostic.status()
       local filename      = statusline.section_filename({ trunc_width = 140 })
       local fileinfo      = statusline.section_fileinfo({ trunc_width = 120 })
-
-      local battery = require("battery").get_status_line()
+      local progress      = vim.ui.progress_status()
+      local is_busy       = vim.bo.busy > 0 and ' 󰜺 ' or ''
+      local battery       = require("battery").get_status_line()
 
       return statusline.combine_groups({
-        { hl = mode_hl,                  strings = { mode } },
-        { hl = 'MiniStatuslineDevinfo',  strings = { git, diff } },
-        -- '%<', -- Truncate point
-        { hl = 'MiniStatuslineFilename', strings = { filename } },
+        { hl = mode_hl,                 strings = { mode } },
+        { hl = 'MiniStatuslineDevinfo', strings = { git } },
+        '%<', -- Truncate here
+        { hl = 'MiniStatuslineFilename', strings = { filename, is_busy } },
         { hl = 'MiniStatuslineDevinfo',  strings = { diagnostics } },
-        '%=', -- Right align split
-        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo, '%p%%' } },
-        { hl = mode_hl,                  strings = { "%l:%c", battery } },
+        '%=', -- Right align
+        { hl = 'MiniStatuslineDevinfo',  strings = { progress } },
+        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+        { hl = mode_hl,                  strings = { battery, "%l:%c" } },
       })
     end,
 
