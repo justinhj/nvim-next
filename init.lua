cfdec5847c2a472401db5c07ffd8f1dfb32f2df1
Justin Heyes-Jones <justinhj@gmail.com>
2026-04-17 17:26:25 -0700

    Agentic nvim

--- init.lua
+++ init.lua
@@ -215,3 +215,12 @@ vim.pack.add({ { src = 'https://codeberg.org/andyg/leap.nvim', version = 'main' 
 
 vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
 vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+
+-- Agentic, experimental
+-- vim.pack.add({ { src = 'https://github.com/carlos-algms/agentic.nvim', version = 'main' } })
+--
+-- local agentic = require('agentic')
+-- local opts = {
+--     provider = 'gemini-acp'
+--   }
+-- agentic.setup(opts)
